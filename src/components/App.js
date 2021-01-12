c10e08fbbf8080ccfd12b040b5000c47ddeadf41
Kelsi Proulx <kelsiproulx1919@gmail.com>
2021-01-12 13:29:59 -0800

    Preliminary work on toast notifications

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
+import {useState} from 'react';
 import '../styles/App.css';
 import oscarIcon from '../images/oscarIcon.svg';
 import Search from './Search';
+import Toast from './Toast';
+import checkIcon from '../images/check.svg';
+import infoIcon from '../images/info.svg';
+
 
 function App() {
+  const [list, setList] = useState([]);
+
+  const testList = [
+    {
+      id: 1,
+      title: 'Success',
+      description: 'This is a success toast component',
+      backgroundColor: '#5cb85c',
+      icon: checkIcon
+    },
+    {
+      id: 2,
+      title: 'Danger',
+      description: 'This is an error toast component',
+      backgroundColor: '#5bc0de',
+      icon: infoIcon
+    },
+  ];
+
   return ( 
     <div className = "App" >
-      <header className = "app-header" >
+      <header className = "app-header" >        
         <h1 className="header-text">The Shoppies</h1>
         <img className="trophy" src={oscarIcon} alt="Oscars trophy" />
       </header>
       <hr className="section-rule" />
+        <Toast toastList={testList} />
         <Search />
     </div>
   );
